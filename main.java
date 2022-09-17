16c6074e6db9af7d62127e22dec7e747f89f03ac
ebayboy123 <107748760+ebayboy123@users.noreply.github.com>
2022-09-17 18:49:57 +0200

    added comments

--- main.java
+++ main.java
@@ -6,39 +6,43 @@ class calculator
 {
     public static void main(String[] args) {
 
+        //variablen
         int num1;
         int num2;
         String method;
 
 
+        //inputs
         Scanner input = new Scanner(System.in);
         System.out.println("enter the first digit:");
         num1 = input.nextInt();
-
         System.out.println("enter the calculatingh method:");
         method = input.next();
-
         System.out.println("enter the second digit:");
         num2 = input.nextInt();
-
         System.out.println("calculating...");
+        
 
-        if (method.equals("+")) {
+        //calc. method
+        if (method.equals("+")) 
+        {
             System.out.println("the result is: "+ (num1 + num2));
         }
-
-        if (method.equals("-")) {
+        if (method.equals("-")) 
+        {
             System.out.println("the result is: "+ (num1 - num2));
         }
-
-        if (method.equals("*")) {
+        if (method.equals("*")) 
+        {
             System.out.println("the result is: "+ (num1 * num2));
         }
-
-        if (method.equals("/")) {
+        if (method.equals("/")) 
+        {
             System.out.println("the result is: "+ (num1 / num2));
         }
+
         
+        //thank you message
         System.out.println("thank you for using my code :)");
 
     }
