1ed4013e09f120c8fa7d66d009d35d1903090e59
ebayboy123 <107748760+ebayboy123@users.noreply.github.com>
2022-09-20 10:25:54 +0200

    added Modulo

--- main.java
+++ main.java
@@ -2,8 +2,9 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-class calculator {
-    public static void main(String[] args){  
+class calculator{
+
+    public static void main(String[] args) {
 
         // variablen
         double num1;
@@ -19,34 +20,34 @@ class calculator {
         System.out.println("enter the second digit:");
         num2 = input.nextDouble();
 
-        //operation
-        if (method.equals("+")) 
-        {
+        // operation
+        if (method.equals("+")) {
             System.out.println("the result is: " + (num1 + num2));
         }
-        if (method.equals("-")) 
-        {
+        if (method.equals("-")) {
             System.out.println("the result is: " + (num1 - num2));
         }
-        if (method.equals("*")) 
-        {
+        if (method.equals("*")) {
             System.out.println("the result is: " + (num1 * num2));
         }
-        if (method.equals("/")) 
-        {
-            if (num2 == 0) 
-            {
+        if (method.equals("/")) {
+            if (num2 == 0) {
                 System.out.println("du bist halt so dumm");
                 System.exit(0);
             }
 
-            else
-            {
-            System.out.println("the result is: " + (num1 / num2));
+            else {
+                System.out.println("the result is: " + (num1 / num2));
             }
+        }
+        if (method.equals("%")) {
+            int value1 = (int) num1;
+            int value2 = (int) num2;
+            System.out.println("the result is: " + (value1 % value2));
+        }
 
         // thank you message
         System.out.println("thank you for wasting your time :)");
-    }
+
     }
 }
